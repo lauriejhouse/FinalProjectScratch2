4e0c78b3bbb1870c29f7f332628b2d8930750d23
JackieN <jackienorstrom@aol.com>
2018-10-19 13:04:06 -0400

    Creating VC
    Creating Master and and Detail View Controller swift files.

--- FinalProjectScratch2/AppDelegate.swift
+++ FinalProjectScratch2/AppDelegate.swift
@@ -10,13 +10,21 @@ import UIKit
 import CoreData
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let splitViewController = self.window?.rootViewController as! UISplitViewController
+        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
+        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
+        splitViewController.delegate = self
+        
+        let masterNavigationController = splitViewController.viewControllers[0] as! UINavigationController
+        let controller = masterNavigationController.topViewController as! MasterViewController
+        controller.managedObjectContext = self.persistentContainer.viewContext
         return true
     }
 
